f1c4f50033dfc81772ebb5ed912505cff33aecf9
Ubuntu <ubuntu@ip-172-31-59-214.ec2.internal>
2021-04-27 14:02:09 +0000

    fixed code and added perimeter

---
+++ basic_math.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python 3
+
+# Created by Lauren Wheatley
+# Created in April 2021
+# This program does basic math
+
+
+def main():
+    # this function does basic math
+    print("l=8, w=7, what is the area of the rectangle?")
+    print("the area is 7*8={}".format(7*8))
+    print("the perimeter 2(7+8)={}".format(2*(7+8)))
+
+
+if __name__ == "__main__":
+    main()
